d4b846317123d91e156a00110a9c081fb55694f5
Jesvin Sebastian Madona <sebastianjesvin@gmail.com>
2021-09-25 13:17:39 +0530

    removed hardcoded values

--- taxibackend/__init__.py
+++ taxibackend/__init__.py
@@ -39,28 +39,28 @@ def create_app():
 	)
 
 #########################################################################################################################
-		
-	@app.route("/")
+
+	@app.route("/", methods=["PUT"])
 	#@cross_origin()
 	def index():
 		response = jsonify(message="The Server is running")
 		return response
-		
-#########################################################################################################################		
-		
-	@app.route("/signin")
+
+#########################################################################################################################
+
+	@app.route("/signin",methods=["POST"])
 	#@cross_origin())
 	def loginpage():
-		cursor = dbconn.cursor()		
-		#phoneno= request.json['phoneno']
-		#password= request.json['pwd']
-	
-		phoneno= '7338995417'
-		password= 'gops123'
-		
+		cursor = dbconn.cursor()
+		phoneno= request.json['phoneno']
+		password= request.json['pwd']
+
+		# phoneno= '7338995417'
+		# password= 'gops123'
+
 		#password encryption:
 		password=hashlib.sha256(password.encode('utf-8')).hexdigest() #hashvalue
-		
+
 		cursor.execute("SELECT phoneno,pwd from Signin_up where phoneno=%s",(phoneno,))
 		temp= cursor.fetchone()
 		if temp:
@@ -74,68 +74,68 @@ def create_app():
 
 #########################################################################################################################
 
-	@app.route("/signup")
+	@app.route("/signup",methods=["POST"])
 	def registration():
 		cursor = dbconn.cursor()
-		#phoneno= request.json['phoneno']
-		#password= request.json['pwd']
-		#typess= request.json['type']
-		
-		phoneno= '7338995417'
-		password= 'gops123'
-		typess= 'rider'
-		
-		
+		phoneno= request.json['phoneno']
+		password= request.json['pwd']
+		typess= request.json['type']
+
+		# phoneno= '7338995417'
+		# password= 'gops123'
+		# typess= 'rider'
+		#
+
 		#password encryption:
 		password=hashlib.sha256(password.encode('utf-8')).hexdigest() #hashvalue
-		
+
 		if typess=='rider':
 			#names= request.json['name']
 			#emails= request.json['email']
-			
+
 			names= 'joseph mani'
 			emails= 'josephmani.uae@gmail.com'
-		
-			
+
+
 			cursor.execute("INSERT INTO Signin_up (phoneno, pwd, type) VALUES(%s, %s, %s)",(phoneno, password, typess))
 			dbconn.commit()
-			
+
 			cursor = dbconn.cursor()
 			cursor.execute("INSERT INTO Rider (name, email, Rphoneno) VALUES(%s, %s, %s)",(names, emails, phoneno))
 			dbconn.commit()
-			
+
 			cursor.execute("SELECT MAX(rid) from Rider")
 			userid= cursor.fetchall()[0][0]
 			dbconn.commit()
-		
+
 			return jsonify({"rid":userid,"name":names,"email":emails})
-		
-	
-	
-########################################  RIDER  ###############################################	
-	
-	@app.route("/book-ride")
+
+
+
+########################################  RIDER  ###############################################
+
+	@app.route("/book-ride", methods=["POST"])
 	def fillnotes():
 		cursor = dbconn.cursor()
-		#phoneno= request.json['phoneno']
-		#froma= request.json['from_add']
-		#toa= request.json['to_add']
-		#times= request.json['time']
-		#shareds= request.json['shared']
-		#vtypess= request.json['vehicletype']
-		#amounts= request.json['amount']
-		
-		phoneno= '7338995417'
-		froma= 'goa'
-		toa= 'bhopal'
-		times='2021-09-25T12:41'
-		shareds= 'T'
-		vtypess= 'suv'
-		amounts= 100
-		
+		phoneno= request.json['phoneno']
+		froma= request.json['from_add']
+		toa= request.json['to_add']
+		times= request.json['time']
+		shareds= request.json['shared']
+		vtypess= request.json['vehicletype']
+		amounts= request.json['amount']
+
+		# phoneno= '7338995417'
+		# froma= 'goa'
+		# toa= 'bhopal'
+		# times='2021-09-25T12:41'
+		# shareds= 'T'
+		# vtypess= 'suv'
+		# amounts= 100
+
 		#2018-06-07T00:00
 		#share ride verification
-	
+
 		if shareds=='T':
 			now = datetime.now()
 			yr=times[2:4]
@@ -156,37 +156,37 @@ def create_app():
 
 
 		otps = random.randint(1000,9999)
-				
+
 		cursor.execute("INSERT INTO CurrentTrip (from_add, to_add, time, shared, vehicletype, amount, otp, Rphoneno) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)",(froma, toa, reqstr, shareds, vtypess, amounts, otps, phoneno))
 		dbconn.commit()
-		
+
 		cursor= dbconn.cursor()
 		cursor.execute("SELECT MAX(tripid) from CurrentTrip where Rphoneno=%s",(phoneno,))
 		tripids= cursor.fetchall()[0][0]
 		dbconn.commit()
-	
+
 		return jsonify({"tripids":tripids, "from_add":froma, "to_add":toa, "time": reqstr, "shared":shareds, "vehicletype":vtypess, "amount":amounts, "otp":otps, "phone":phoneno})
-	
+
 
 #########################################################################################################################
 
 	@app.route("/getallnotes", methods=["GET"])
 	def getnotes():
 		cursor = dbconn.cursor()
-		userid= request.json['id']		
-		
+		userid= request.json['id']
+
 		cursor.execute("SELECT nid,title,dcp,dates,hids from notes where uid= %s",(userid,))
 		temp= cursor.fetchall()
-		
+
 		result=[]
 		for notesid,ntitle,description,dated,hashtag_ids in temp:
 				dicts={"notesid":notesid, "title":ntitle, "description":description, "date":dated, "hashtags":hashtag_ids,"id":userid}
 				result.append(dicts)
-	
+
 		return jsonify(result)
 
 #########################################################################################################################
-		
+
 	@app.route("/update", methods=["PUT"])
 	def updatenote():
 		dated=str(date.today())
@@ -197,15 +197,15 @@ def create_app():
 		description= request.json['description']
 		hashtag_ids= request.json['hashtags']
 		hash_id=[v.strip() for v in hashtag_ids.split(',')]
-		
-		
+
+
 		cursor = dbconn.cursor()
 		cursor.execute("UPDATE notes SET title=%s, dcp=%s, dates=%s, hids=%s where nid=%s and uid=%s",(ntitle, description, dated, hashtag_ids, notesid, userid))
 		cursor.execute("DELETE FROM hashtags where nid=%s and uid=%s",(notesid, userid))
 		dbconn.commit()
-		
+
 		hashidlist=[]
-		for hashes in hash_id:	
+		for hashes in hash_id:
 				cursor = dbconn.cursor()
 				cursor.execute("INSERT INTO hashtags (label,nid,uid) SELECT %s, %s, %s where NOT exists( select 1 from hashtags where label=%s)",(hashes, notesid, userid, hashes))
 				dbconn.commit()
@@ -214,32 +214,32 @@ def create_app():
 				hashids= cursor.fetchall()[0][0]
 				hashidlist.append(hashids)
 				dbconn.commit()
-		
+
 		cursor = dbconn.cursor()
 		cursor.execute("SELECT title,dcp,dates,hids from notes where nid= %s and uid=%s",(notesid,userid))
 		values= cursor.fetchall()
 		dbconn.commit()
-		return jsonify({"notesid":notesid, "title":ntitle, "description":description, "date":dated, "hashtags":hashtag_ids, "hashtagids": hashidlist ,"id":userid})	
-		
-#########################################################################################################################		
-				
+		return jsonify({"notesid":notesid, "title":ntitle, "description":description, "date":dated, "hashtags":hashtag_ids, "hashtagids": hashidlist ,"id":userid})
+
+#########################################################################################################################
+
 	@app.route("/delete", methods=["DELETE"])
 	def deletenote():
 		cursor = dbconn.cursor()
 		userid= request.json['id']
 		notesid= request.json['notesid']
-		
+
 		cursor.execute("SELECT title,dcp,dates,hids from notes where uid= %s and nid=%s",(userid,notesid))
 		temp= cursor.fetchone()
 		ntitle,description,dated,hashtag_ids=temp
 		dbconn.commit()
-		
+
 		cursor = dbconn.cursor()
 		cursor.execute("SELECT hids from notes where uid= %s and nid=%s",(userid,notesid))
 		hashidlist= cursor.fetchone()
 		dbconn.commit()
-		
-		
+
+
 		cursor = dbconn.cursor()
 		cursor.execute("DELETE FROM hashtags where nid=%s and uid=%s",(notesid, userid))
 		cursor.execute("DELETE FROM notes where nid=%s and uid=%s",(notesid, userid))
@@ -247,7 +247,7 @@ def create_app():
 		return jsonify({"notesid":notesid, "title":ntitle, "description":description, "date":dated, "hashtags":hashtag_ids, "hashtagids": hashidlist ,"id":userid})
 
 #########################################################################################################################
-	
+
 	@app.route("/hashtags", methods=["GET"])
 	def gethashtags():
 		cursor = dbconn.cursor()
@@ -259,8 +259,7 @@ def create_app():
 		for hashid,hashes in values:
 			dicts={"hashtagid":hashid, "hashtag":hashes}
 			result.append(dicts)
-		
-		return jsonify(result)		
-	
+
+		return jsonify(result)
+
 	return app
-	
